8fd0428a3b99ad81c5845a339886d61ad1e1d864
windopper <sevenstarpleiades@gmail.com>
2023-12-20 21:36:34 +0900

    fixed untracked files

--- dist/api/test/youtube.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const ytdl_core_1 = __importDefault(require("ytdl-core"));
-ytdl_core_1.default.getInfo("https://www.youtube.com/watch?v=-o7X4W-wQbo").then(console.log);
